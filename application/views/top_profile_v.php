c7cfa12e5a3d09e5773e04b47276f6ebe3c568a5
rizafr <riza.fauzi.rahman@gmail.com>
2015-03-26 08:47:18 +0700

    ubah box user dan detail akses ubah loading

--- application/views/top_profile_v.php
+++ application/views/top_profile_v.php
@@ -1,5 +1,7 @@
 <div id="top_profile">
-	Hi, <strong><?php echo $this->session->userdata('nama_lengkap'); ?></strong><br />
-    <div class="clear"></div>
-	Akses : <i> <?php echo $this->session->userdata('level'); ?> </i>
+	<img class="user" src="<?php echo base_url(); ?>files/images/user.png" />
+	<div class="user-detail">
+		Hi, <b><?php echo $this->session->userdata('nama_lengkap'); ?></b><br />
+		Akses : <b><i> <?php echo $this->session->userdata('level'); ?> </i></b>
+	</div>
 </div>
