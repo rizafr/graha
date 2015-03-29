49b2579f4515a2bde69d6fabaceac5dc65dd936b
rizafr <riza.fauzi.rahman@gmail.com>
2015-03-29 18:37:49 +0700

    perbaikan login/css/style.css

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -1,38 +1,39 @@
 <?php echo $header; ?>
-
-    <span href="#" class="button" id="toggle-login">E- Marketing <br /> PT. Pembangunan Jaya Ancol Tbk</span>
-    
-	<div id="login">
-	
-		<h1>Silakan Login</h1>	
-			<?php
-				if($this->session->flashdata('error') != "")
-				{
-					?>
-					<div class="peringatan" style="z-index:10000"> <?php echo $this->session->flashdata('error'); ?> </div>
-					<?php
-				}
+	<div class="konten">
+		<span href="#" class="button" id="toggle-login">E- Marketing <br /> PT. Pembangunan Jaya Ancol Tbk</span>
+		
+		<div id="login">
+		
+			<h1>Silakan Login</h1>	
+				<?php
+					if($this->session->flashdata('error') != "")
+					{
+						?>
+						<div class="peringatan" style="z-index:10000"> <?php echo $this->session->flashdata('error'); ?> </div>
+						<?php
+					}
+					
+					if($this->session->flashdata('success') != "")
+					{
+						?>
+						<div class="peringatan" style="z-index:10000"> <?php echo $this->session->flashdata('success'); ?> </div>
+						<?php
+					}
+					?> 
 				
-				if($this->session->flashdata('success') != "")
-				{
-					?>
-					<div class="peringatan" style="z-index:10000"> <?php echo $this->session->flashdata('success'); ?> </div>
-					<?php
-				}
-				?> 
-			
-			<form id="form1" action="<?php echo base_url(); ?>login/process" method="post" autocomplete="off">
-			 	<input type="text" name="username" value="" placeholder="masukan username" required />
-				<input type="password" name="password" value="" placeholder="masukan password" required/>
-				<input name="captcha" type="captcha" required title="Silakan Isi Capctha" />
-						<div class="captcha"><?php echo $captcha; ?></div>
-				<p><span>&nbsp;</span><input class="submit" type="submit" name="name" value="login" title="Klik jika sudah mengisi username dan password"/></p>
-			 </form>				
-    </div>
+				<form id="form1" action="<?php echo base_url(); ?>login/process" method="post" autocomplete="off">
+					<input type="text" name="username" value="" placeholder="masukan username" required />
+					<input type="password" name="password" value="" placeholder="masukan password" required/>
+					<input name="captcha" type="captcha" required title="Silakan Isi Capctha" />
+							<div class="captcha"><?php echo $captcha; ?></div>
+					<p><span>&nbsp;</span><input class="submit" type="submit" name="name" value="login" title="Klik jika sudah mengisi username dan password"/></p>
+				 </form>				
+		</div>
+   
     <footer>
 		<p>PT. Pembangunan Jaya Ancol Tbk</p>
     </footer> 
-  
+   </div>
   <!-- javascript at the bottom for fast page loading -->
   <script src="<?php echo base_url(); ?>files/login/js/index.js"></script>  
   <script>
